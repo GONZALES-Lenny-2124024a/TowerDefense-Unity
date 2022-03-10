cd45acb1e1c2546e1e2d16150d69ac45b22dd29d
Lenny Gonzales <lenny.gonzales@etu.univ-amu.fr>
2022-03-10 13:17:22 +0100

    Move one enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -17,6 +17,18 @@ public class Enemy : MonoBehaviour
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f) {
+            GetNextWaypoint();
+        }
+    }
 
+    void GetNextWaypoint() {
+        if (wavePointIndex >= Waypoints.waypoints.Length -1) {
+            Destroy (gameObject);
+            return;
+        }
+
+        ++wavePointIndex;
+       target = Waypoints.waypoints[wavePointIndex];
     }
 }
