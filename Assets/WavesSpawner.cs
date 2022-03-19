a041783da9a434cd1856bbc5b136463adef827d1
Lenny Gonzales <lenny.gonzales@etu.univ-amu.fr>
2022-03-19 14:52:51 +0100

    Waves with enemies

--- Assets/WavesSpawner.cs
+++ Assets/WavesSpawner.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 
 public class WavesSpawner : MonoBehaviour
 {
-    public Transform enemyPreFab;
+    public Transform enemyPreFabCommon;
+    public Transform enemyPreFabRare;   //ND
+    public Transform enemyPreFabEpic;   //ND
+    public Transform enemyPreFabLegendary;  //ND
+    public Transform enemyPreFabGeneric;    //ND
 
-    public Transform spawnPoint;
+    //Create an array with all enemies' type        ND
+    //private Transform[] ennemies = {enemyPreFabCommon, enemyPreFabRare, enemyPreFabEpic, enemyPreFabLegendary, enemyPreFabGeneric};
+    private int number;
+
+    public Transform spawnPoint;    //For the position of the enemy
 
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
-
     public Text waveCountDownText;
 
     private int waveIndex = 0;
+    private Text waveIndexText; //ND
 
     void Update() {
         if (countdown <= 0f) {
@@ -36,7 +44,22 @@ public class WavesSpawner : MonoBehaviour
         }
     }
 
-    void SpawnEnemy() {
-        Instantiate(enemyPreFab, spawnPoint.position, spawnPoint.rotation);
+    void SpawnEnemy() {     //ND
+        number = Random.Range(0, 100); //returns random number between 0-99
+        if (number < 50) {
+            Instantiate(enemyPreFabCommon, spawnPoint.position, spawnPoint.rotation);   //Spawn Common Ennemy
+        }
+        else if (number >= 50 && number < 80) {
+            Instantiate(enemyPreFabRare, spawnPoint.position, spawnPoint.rotation);   //Spawn Rare Ennemy
+        }
+        else if (number >= 80 && number < 92) {
+            Instantiate(enemyPreFabEpic, spawnPoint.position, spawnPoint.rotation);   //Spawn Epic Ennemy
+        }
+        else if (number >= 92 && number < 98) {
+            Instantiate(enemyPreFabLegendary, spawnPoint.position, spawnPoint.rotation);   //Spawn Legendary Ennemy
+        }
+        else {
+            Instantiate(enemyPreFabGeneric, spawnPoint.position, spawnPoint.rotation);   //Spawn Generic Ennemy
+        }      
     }
 }
